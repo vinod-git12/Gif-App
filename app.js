0334cc8ef49aa05bbc36850f156e23d0f8913ce3
vinod kumar <vinod513kumar@gmail.com>
2021-01-04 16:00:53 -0500

    Focusing on CSS

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 
+let selectMenu = document.querySelector(".user-rating") 
 const getData = () => {
 
   let form1 = document.querySelector('.form')
   form1.addEventListener("submit",  async(event) => {
     event.preventDefault()
+    let clearImg = document.querySelector('#results')
+    clearImg.innerHTML = ''
     let imageOne = document.querySelector('.image1')
-    const choiceOne = document.querySelector('.choice').value
-    const weirdLevel = document.querySelector("#weirdlevel").value
+    const userInput = document.querySelector('.user-input').value
+    const userRating = selectMenu.value
+    console.log(selectMenu.value)
 
-    let response = await axios.get(`https://api.giphy.com/v1/gifs/search?api_key=N2dljXKrv7SaEJ1R8KWvr8i8yUOFltOl&q=${choiceOne}&weirdness=${weirdLevel}`)
-    console.log(weirdLevel)
+    let response = await axios.get(`https://api.giphy.com/v1/gifs/search?api_key=N2dljXKrv7SaEJ1R8KWvr8i8yUOFltOl&q=${userInput}&rating=${userRating}`)
+    //console.log(weirdLevel)
     console.log(response)
     console.log(typeof response.data.data)
     
@@ -19,7 +23,7 @@ const getData = () => {
   })
 
 }
-
+getData()
 
 const renderImages = (results) => {
   for (let i = 0; i < 3; i++) {
@@ -33,7 +37,31 @@ const renderImages = (results) => {
   
 }
 
-getData()
+const ratingList = () => {
+  
+  console.log(selectMenu)
+  let arrayList = ['g', 'pg', 'pg-13', 'r']
+  for (let i = 0; i < arrayList.length; i++) {
+    let optionList = document.createElement("option")
+    optionList.value = arrayList[i]
+    optionList.innerText = arrayList[i]
+    selectMenu.append(optionList)
+    
+  }
+  // selectMenu.addEventListener("change", () => {
+  //   //console.log(selectMenu.value)
+    
+  // })
+
+}
+ratingList()
+
+
+
+
+
+
+
 
 
 
